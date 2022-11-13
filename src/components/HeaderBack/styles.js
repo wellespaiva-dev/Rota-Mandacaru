87b47527ec7b361ee39dcd8301daaa344754a7f7
rosluc <rosendolukas09@gmail.com>
2022-11-13 16:26:03 -0300

    Correção de estilo no componete HeaderBack

--- src/components/HeaderBack/styles.js
+++ src/components/HeaderBack/styles.js
@@ -1,28 +1,22 @@
 import styled from "styled-components";
-import {Dimensions} from "react-native";
-
-const {width} = Dimensions.get('window')
 
 export const Container = styled.View`
   width: 100%;
   flex-direction: row;
   background-color: #FFFFFF;
-  height: 64px;
-  padding: 15px;
   align-items: center;
   justify-content: center;
+  z-index: 1;
 `;
 
 export const Title = styled.Text`
+  padding: 30px 15px 15px 15px;
   font-size: 22px;
   color: black;
 `;
 
 export const ButtonBack = styled.TouchableOpacity`
+  padding: 30px 15px 15px 15px;
   position: absolute;
   left: 10px;
-  width: 50px;
-  height: 50px;
-  align-items: center;
-  justify-content: center;
 `;
